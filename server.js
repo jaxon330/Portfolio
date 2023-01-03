8afeb416217a69440c8af9115e891c0b38e95da8
john-330 <jaxon330@gmail.com>
2023-01-02 23:42:05 -0600

    new update2

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const path = require("path")
+
 const PORT = 5000
 app.use(express.static('public'));
-// app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
-    res.sendFile('/index.html', {root : __dirname})
-    // res.send('hello world')
-})
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  })
+
 // app.get('/', (req, res) => res.send('Home page Route!'))
 app.listen(PORT, (req, res) => console.log('Server is running on port 5000'))
