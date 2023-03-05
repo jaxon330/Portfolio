4be2f55940a3389a99841fc7db1cdb2b9db9afd9
john-330 <jaxon330@gmail.com>
2023-03-05 13:59:11 -0600

    new update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require("path")
 
-const PORT = 5000
+const PORT = 3000
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
   })
 
 // app.get('/', (req, res) => res.send('Home page Route!'))
-app.listen(PORT, (req, res) => console.log('Server is running on port 5000'))
+app.listen(PORT, (req, res) => console.log('Server is running on port 3000'))
